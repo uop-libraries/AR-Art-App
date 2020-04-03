a2a1e90461aa773376515b5563ea4fe3b372141b
khcanniff <k_canniff@u.pacific.edu>
2020-04-03 10:06:06 -0700

    text reader and dictionary done

--- Lisa AR Art/Assets/Scripts/TextReader.cs
+++ Lisa AR Art/Assets/Scripts/TextReader.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
 
+/**
+ * reads in text file and splits text by newline into array lists
+ * each string is added to a dictionary to store the text observations
+ */
 public class TextReader : MonoBehaviour
 {
+    private IDictionary<int, string> dict;
+
+    void Awake()
+    {
+        dict = new Dictionary<int, string>();
+    }
     public void readTextFile()
     {
         string filePath = "Assets/TextFile/test.txt";
         StreamReader reader = new StreamReader(filePath);
-        string test = reader.ReadToEnd();
-        Debug.Log(test); //print the text out
+        string text = reader.ReadToEnd();
         reader.Close(); //close the reader
+        this.addStringToDictionary(text);
 
     }
 
@@ -20,5 +30,27 @@ public class TextReader : MonoBehaviour
     void Update()
     {
         
+    }
+
+    /**addStringToDictionary
+     * add each line to a dictionary and remove extra white spaces
+     */
+    void addStringToDictionary(string text)
+    {
+        var lines = text.Split("\n"[0]);
+        for(int i = 0; i < lines.Length; i++)
+        {
+            if (!string.IsNullOrWhiteSpace(lines[i]))
+            {
+                Debug.Log(lines[i]);
+                dict.Add(i, lines[i]);
+            }
+        }
+        
+    }
+
+    public IDictionary<int, string> getDict()
+    {
+        return dict;
     }
 }
