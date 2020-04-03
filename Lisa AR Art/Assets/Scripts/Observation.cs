8057f658703bab1b6c7af5ee99d597161ab6faf7
khcanniff <k_canniff@u.pacific.edu>
2020-04-03 11:01:02 -0700

    created observation class and handler

---
+++ Lisa AR Art/Assets/Scripts/Observation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observation : MonoBehaviour
+{
+    public List<GameObject> visuals = new List<GameObject>();
+    public List<AudioClip> clips = new List<AudioClip>();
+    public int classYear;
+    private int observationID;
+    private AudioSource audioSpeaker;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audioSpeaker = this.GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setObservationID(int theID)
+    {
+        observationID = theID;
+    }
+
+    public void playObservation()
+    {
+
+    }
+
+    public void pauseObservation()
+    {
+
+    }
+
+    public void setObservation()
+    {
+
+    }
+}
---
+++ Lisa AR Art/Assets/Scripts/ObservationHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObservationHandler : MonoBehaviour
+{
+    private int currentPlayingObservation;
+    private List<GameObject> listOfObservations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<GameObject> listOfObservations = new List<GameObject>();
+        currentPlayingObservation = 0;
+        setUp();
+    }
+
+    void setUp()
+    {
+        foreach (GameObject foundObservation in GameObject.FindGameObjectsWithTag("observation"))
+        {
+
+            listOfObservations.Add(foundObservation);
+        }
+    }
+
+    public void playNextObservation()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
