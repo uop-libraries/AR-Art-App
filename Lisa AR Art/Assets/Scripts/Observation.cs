1bf96d1619e1f3b0e820fec6cc5cea5e818ad7b4
khcanniff <k_canniff@u.pacific.edu>
2020-04-09 15:16:41 -0700

    build for demo

--- Lisa AR Art/Assets/Scripts/Observation.cs
+++ Lisa AR Art/Assets/Scripts/Observation.cs
@@ -6,17 +6,23 @@ using UnityEngine.UI;
 public class Observation : MonoBehaviour
 {
     public GameObject visuals;
-    public List<AudioClip> clips = new List<AudioClip>();
+    public bool waitForVoiceToBeDone; //TRUE if you want the visuals to appear AFTER the narrating voice is done
+    public List<AudioClip> bgAudioClips = new List<AudioClip>();
+    public AudioClip voiceClip; //voice narrating the text
     private Text [] observationTextReference;
     private string classYear;
     private string observationID;
     private string observationText;
-    private AudioSource audioSpeaker;
-
+    private AudioSource audioSpeakerForBGSounds;
+    private AudioSource audioSpeakerForVoice;
+    private int bgSongIndex = 0;
     void Awake()
     {
-        audioSpeaker = this.GetComponent<AudioSource>();
+        audioSpeakerForBGSounds = this.GetComponent<AudioSource>();
+        audioSpeakerForVoice = this.gameObject.AddComponent<AudioSource>(); //audio source for the voice
+        audioSpeakerForVoice.clip = voiceClip;
         observationTextReference = this.GetComponentsInChildren<Text>(true); //include inactives
+        visuals.SetActive(false);
     }
 
     /** setObservationID
@@ -42,10 +48,16 @@ public class Observation : MonoBehaviour
     public void playObservation()
     {
         //this.setObservationText(observationText);
-        this.gameObject.SetActive(true);
-        visuals.SetActive(true);
-        observationTextReference[0].transform.parent.gameObject.GetComponent<ScrollText>().resetScrollBarPosition(); //reset the scroll bar
-        observationTextReference[0].transform.parent.gameObject.GetComponent<ScrollText>().setIsScrolling(true); //get the parent of text to start auto scroll
+        if (!waitForVoiceToBeDone)
+        {
+            bgSongIndex = 0;
+            //Debug.Log("set the object active!");
+            this.gameObject.SetActive(true);
+            visuals.SetActive(true);
+            observationTextReference[0].transform.parent.gameObject.GetComponent<ScrollText>().resetScrollBarPosition(); //reset the scroll bar
+            observationTextReference[0].transform.parent.gameObject.GetComponent<ScrollText>().setIsScrolling(true); //get the parent of text to start auto scroll
+        }
+        this.playSounds();
 
     }
 
@@ -76,5 +88,40 @@ public class Observation : MonoBehaviour
         return "observationID: " + observationID + " classYear: " + classYear + "observationText: " + observationText;
     }
 
- 
+    /**playSounds
+     * Handles playing of music/sounds
+     * if waitForVoiceToBeDone is true, then wait for the voice to be done to start the BG sounds and activates the visuals
+     */
+    private void playSounds()
+    {
+        audioSpeakerForVoice.Play();
+        if (!(waitForVoiceToBeDone)) //waitForVoiceToBeDone
+        {
+            visuals.SetActive(true); //activate the visuals
+            this.playNextTrack(); //play BG sounds
+        }
+        else
+        {
+            waitForVoiceToBeDone = false;
+            Invoke("playSounds", audioSpeakerForVoice.clip.length + 0.5f); //call method again when voice is done to start the other BG sounds
+        }
+
+    }
+
+    /**playNextTrack
+     * plays audio and invokes this method again if there is another audio clip to play
+     */
+    private void playNextTrack()
+    {
+        Debug.Log("bgSongIndex = " + bgSongIndex);
+        audioSpeakerForBGSounds.clip = bgAudioClips[bgSongIndex];
+        audioSpeakerForBGSounds.Stop(); // just in case
+        audioSpeakerForBGSounds.Play(); //play the song
+        bgSongIndex++;
+        if (bgSongIndex < bgAudioClips.Count)
+        {
+            Invoke("playNextTrack", audioSpeakerForBGSounds.clip.length + 0.5f); //call method again when sound is done
+        }
+    }
+
 }
